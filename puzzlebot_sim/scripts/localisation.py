568a0ab2e83e644d1dfd7ed86c15704f934fb5b3
CarlosOX <84168967+CarlosOX@users.noreply.github.com>
2024-05-09 12:23:24 -0600

    Update localisation.py

--- puzzlebot_sim/scripts/localisation.py
+++ puzzlebot_sim/scripts/localisation.py
@@ -11,6 +11,11 @@ from tf.transformations import quaternion_from_euler
 import numpy as np 
 
 
+np.set_printoptions(suppress=True) 
+
+np.set_printoptions(formatter={'float': '{: 0.4f}'.format}) 
+
+
 #This class will do the following: 
 
 #   subscribe to the /wr and /wl topic  
@@ -22,6 +27,7 @@ class Localisation():
         # first thing, init a node! 
 
         rospy.init_node('localisation') 
+        
 
         ###******* INIT PUBLISHERS *******###  
 
@@ -38,7 +44,7 @@ class Localisation():
 
         self.r=0.05 #puzzlebot wheel radius [m] 
         self.L = 0.19 #puzzlebot wheel separation [m] 
-        self.dt = 0.02 # Desired time to update the robot's pose [s] 
+        self.dt = 0.1 # Desired time to update the robot's pose [s] 
 
         ############ Variables ############### 
 
@@ -50,20 +56,81 @@ class Localisation():
         self.wr = 0.0
         self.wl = 0.0
 
+        ########################################### DEAD RECKONING ############################
+
+        ######### CONTROL VARIABLES FOR Q ##########
+        #self.wr_k = 0.15
+
+        #self.wl_k = 0.2
+
+        self.wr_k = 0.2
+
+        self.wl_k = 0.8
+
+        ###################
+
+        self.z_covariance = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]])
+
+        self.miu = np.array([0, 0, 0])
+
+        self.w_sigma_const = 0.5 * self.dt * self.r
+        
         self.odom = Odometry() 
 
         rate = rospy.Rate(int(1.0/self.dt)) # The rate of the while loop will be the inverse of the desired delta_t 
 
         while not rospy.is_shutdown(): 
-            ####### Add / modify  your code here ############# 
 
+            #####OBTENER VELOCIDADES #####################
             [self.v, self.w] = self.get_robot_vel(self.wr,self.wl)
+
+            ####### DEAD RECKONING ########################
+
+            self.sigma_k = np.array([[self.wr_k*self.wr, 0],
+                                     [0,  self.wl_k*self.wl]])
+        
+    
+
+            self.w_sigma = self.w_sigma_const * (np.array([[np.cos(self.miu[2]),np.cos(self.miu[2])],
+                                                                [np.sin(self.miu[2]),np.sin(self.miu[2])],
+                                                                [2/self.L, 2/self.L]]))
+        
+        
+            self.Q_k =  self.w_sigma @ self.sigma_k @ self.w_sigma.T
+
+
+            
+            self.H = np.array([[1, 0, -(((self.wl + self.wr) *0.5 *self.r*self.dt )* np.sin(self.miu[2]))],
+                                   [0, 1,(((self.wl + self.wr) *0.5 *self.r*self.dt )* np.cos(self.miu[2]))],
+                                   [0, 0, 1]])
+            
+
+
+            self.miu = np.array([self.miu[0] + (((self.wl + self.wr) *0.5 *self.r*self.dt )* np.cos(self.miu[2])),
+                                     self.miu[1] + (((self.wl + self.wr) *0.5 *self.r*self.dt )* np.sin(self.miu[2])),
+                                     self.miu[2] + (((self.wr - self.wl)/self.L)*self.r*self.dt)])
+            
+            
+
+            self.z_covariance = (self.H @ self.z_covariance @ self.H.T) + self.Q_k
+
+
+
+            ####################################################
+
+           
             self.update_robot_pose(self.v, self.w) 
-            odom_msg = self.get_odom_stamped(self.x, self.y, self.theta) 
+            odom_msg = self.get_odom_stamped(self.x, self.y, self.theta,self.z_covariance,self.v,self.w) 
             odom_msg.twist.twist.linear.x = self.v
             odom_msg.twist.twist.angular.z = self.w
 
-            print(odom_msg)
+            print(self.wr)
+            print(self.wl)
+
+            print(self.miu)
+        
+
+
 
             ######################################### 
             ######## Publish the data ################# 
@@ -87,7 +154,7 @@ class Localisation():
         return [v, w] 
 
 
-    def get_odom_stamped(self, x, y, yaw): 
+    def get_odom_stamped(self, x, y, yaw,Sigma,Mu_v,Mu_w): 
         # x, y and yaw are the robot's position (x,y) and orientation (yaw) 
         # Write the data as a ROS PoseStamped message 
 
@@ -104,6 +171,18 @@ class Localisation():
         odom_stamped.pose.pose.orientation.y = quat[1]
         odom_stamped.pose.pose.orientation.z = quat[2]
         odom_stamped.pose.pose.orientation.w = quat[3]
+
+        odom_array = np.array([[Sigma[0][0],Sigma[0][1],0,0,0,Sigma[0][2]],
+                       [Sigma[1][0],Sigma[1][1],0,0,0,Sigma[1][2]],
+                       [0,0,0,0,0,0],
+                       [0,0,0,0,0,0],
+                       [0,0,0,0,0,0],
+                       [Sigma[2][0],Sigma[2][1],0,0,0,Sigma[2][2]]])
+        
+        odom_stamped.pose.covariance = odom_array.flatten().tolist()
+
+        odom_stamped.twist.twist.linear.x = Mu_v
+        odom_stamped.twist.twist.angular.z = Mu_w
         
         return odom_stamped 
 
